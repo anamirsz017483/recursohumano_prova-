f9d7bf5e061438a3408f3af86902c3a25c0977cf
anamirsz017483 <anarosa.miranda1a@gmail.com>
2025-09-24 15:01:49 -0300

    Add files via upload

---
+++ item_pedido.sql
@@ -0,0 +1,74 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Tempo de geração: 24/09/2025 às 20:03
+-- Versão do servidor: 10.4.32-MariaDB
+-- Versão do PHP: 8.2.12
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Banco de dados: `estoque`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura para tabela `item_pedido`
+--
+
+CREATE TABLE `item_pedido` (
+  `id_pedido` int(11) NOT NULL,
+  `id_produto` int(11) NOT NULL,
+  `quantidade` int(11) DEFAULT NULL,
+  `preco_unitario` decimal(10,2) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Despejando dados para a tabela `item_pedido`
+--
+
+INSERT INTO `item_pedido` (`id_pedido`, `id_produto`, `quantidade`, `preco_unitario`) VALUES
+(1, 1, 2, 3500.00),
+(2, 2, 1, 800.00),
+(2, 3, 2, 80.00),
+(3, 4, 2, 750.00),
+(4, 5, 6, 300.00),
+(5, 1, 1, 3500.00);
+
+--
+-- Índices para tabelas despejadas
+--
+
+--
+-- Índices de tabela `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD PRIMARY KEY (`id_pedido`,`id_produto`),
+  ADD KEY `id_produto` (`id_produto`);
+
+--
+-- Restrições para tabelas despejadas
+--
+
+--
+-- Restrições para tabelas `item_pedido`
+--
+ALTER TABLE `item_pedido`
+  ADD CONSTRAINT `item_pedido_ibfk_1` FOREIGN KEY (`id_pedido`) REFERENCES `pedido_compra` (`id_pedido`),
+  ADD CONSTRAINT `item_pedido_ibfk_2` FOREIGN KEY (`id_produto`) REFERENCES `produto` (`id_produto`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
